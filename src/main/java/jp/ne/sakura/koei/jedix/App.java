cca66a36eb75a5a480e798ef321dc55c7bcd30f1
koe6601sr <koe.6601sr@gmail.com>
2014-05-21 16:57:16 +0900

    initial commit

---
+++ src/main/java/jp/ne/sakura/koei/jedix/App.java
@@ -0,0 +1,13 @@
+package jp.ne.sakura.koei.jedix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
